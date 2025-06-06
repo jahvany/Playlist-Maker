0ebdcf36a48e3bf46b3cd58634c6ed92531228a9
Jahvany <teresch27@gmail.com>
2025-06-06 22:37:19 +0200

    1

--- app/src/main/java/com/practicum/playlistmaker/SettingActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingActivity.kt
@@ -1,21 +1,72 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textview.MaterialTextView
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val titleSettings = findViewById<MaterialToolbar>(R.id.titleSettings)
-        setSupportActionBar(titleSettings)
-        supportActionBar?.title = getString(R.string.settings)
         titleSettings.setNavigationOnClickListener {finish()}
+
+        val themeSwitch = findViewById<SwitchMaterial>(R.id.themeSwitch)
+        val isDark = when (AppCompatDelegate.getDefaultNightMode()) {
+            AppCompatDelegate.MODE_NIGHT_YES -> true
+            AppCompatDelegate.MODE_NIGHT_NO -> false
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM,
+            AppCompatDelegate.MODE_NIGHT_UNSPECIFIED -> {
+                val uiMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+                uiMode == Configuration.UI_MODE_NIGHT_YES
+            }
+            else -> false
+        }
+        themeSwitch.isChecked = isDark
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            AppCompatDelegate.setDefaultNightMode(
+                if (isChecked)
+                    AppCompatDelegate.MODE_NIGHT_YES
+                else
+                    AppCompatDelegate.MODE_NIGHT_NO
+            )
+            recreate()
+        }
+
+        val shareMessage = "https://practicum.yandex.ru/profile/android-developer-plus"
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, shareMessage)
+        }
+
+        val supportSubject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+        val supportMessage = "Спасибо разработчикам и разработчицам за крутое приложение!"
+        val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("jarman@bk.ru"))
+            putExtra(Intent.EXTRA_SUBJECT, supportSubject)
+            putExtra(Intent.EXTRA_TEXT, supportMessage )
+        }
+
+        val agreementIntent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+        }
+
+        val shareButton = findViewById<MaterialTextView>(R.id.shareButton)
+        shareButton.setOnClickListener {startActivity(shareIntent)}
+
+        val supportButton = findViewById<MaterialTextView>(R.id.supportButton)
+        supportButton.setOnClickListener {startActivity(supportIntent)}
+
+        val agreementButton = findViewById<MaterialTextView>(R.id.agreementButton)
+        agreementButton.setOnClickListener {startActivity(agreementIntent)}
     }
 }
